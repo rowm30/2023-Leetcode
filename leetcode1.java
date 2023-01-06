9c7be9c9a9131c8bcc92d061e02f0f7a8929c56e
rowm30 <rowm30@gmail.com>
2023-01-07 01:38:33 +0530

    LeetcodeQ1_Done

---
+++ leetcode1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class leetcode1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[n][m];
+        System.out.println(arr.length);
+        System.out.println(arr[0].length);
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
